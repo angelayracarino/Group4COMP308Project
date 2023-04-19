589567955976c077bf0f6c57aaca979ed153dda8
angelayracarino <acarino3@my.centennialcollege.ca>
2023-04-19 14:10:35 -0400

    merged from apple

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -17,6 +17,15 @@ import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
+import { UserContext } from './shared/UserContext';
+import AlertList from './components/AlertList';
+import CreateAlert from './components/CreateAlert';
+import CreateSymptom from './components/CreateSymptom';
+
+import EditVital from './components/EditVital';
+import EditTip from './components/EditTip';
+import Game from './components/Game';
+
 //
 function App() {
 
@@ -31,6 +40,15 @@ function App() {
             <Nav className="ml-auto">
               {/* <Nav.Link as={Link} to="/register">Register</Nav.Link> */}
               <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/home">Home</Nav.Link>
+              <Nav.Link as={Link} to="/alert-list">Alert List</Nav.Link>
+              <Nav.Link as={Link} to="/create-alert">Create Alert</Nav.Link>
+              <Nav.Link as={Link} to="/create-symptom">Create Symptom</Nav.Link>
+              <Nav.Link as={Link} to="/edit-vital">Edit Vital</Nav.Link>
+              <Nav.Link as={Link} to="/edit-tip">Edit Tip</Nav.Link>
+              <Nav.Link as={Link} to="/game">Game</Nav.Link>
+
+
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -40,6 +58,14 @@ function App() {
         <Routes>
           <Route path="/login" element={< Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/alert-list" element={<AlertList />} />
+          <Route path="/create-alert" element={<CreateAlert />} />
+          <Route path="/create-symptom" element={<CreateSymptom />} />
+          <Route path="/edit-vital" element={<EditVital />} />
+          <Route path="/edit-tip" element={<EditTip />} />
+          <Route path="/game" element={<Game />} />
+
         </Routes>
       </div>
 
